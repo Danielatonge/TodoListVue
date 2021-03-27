454a420ddf920ee5650492a40400f42f6e7fc6ff
elamboatonge <elamboatonge@gmail.com>
2021-03-28 02:51:33 +0300

    integrates fake rest api with todo tracker

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+      <h2>Task Tracker </h2>
+      <Button :text="!toggle ? 'Add Task': 'Close'" :color="!toggle ? 'green': 'red'"  @btn-click="$emit('toggle-add-task')" />
+  </header>
+</template>
+
+<script>
+import Button from './Button.vue'
+export default {
+  components: { Button },
+    name: 'Header',
+    methods: {
+    },
+    props: {
+        toggle: Boolean
+    }
+}
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
